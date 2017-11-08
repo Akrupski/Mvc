a8fe764d10a4de1a2e7e9606ad6e25aa9f03d1c7
Akrupski <olo93.pl@gmail.com>
2017-11-08 23:41:18 +0100

    Add project files.

---
+++ OdeTooFood/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using OdeTooFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace OdeTooFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var model = new AboutModel();
+            model.Name = "Olek";
+            model.Location = "Warszawa, Poland";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
